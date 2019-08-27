b1e4c30dc940e6e0dda97a8ba304b070ecfa4df6
lampart_vi_hoang_son <vihoangson@gmail.com>
2019-08-27 22:59:59 +0700

    ddd

--- scripts/app.js
+++ scripts/app.js
@@ -80,7 +80,6 @@ $(".click-more-img").click(function (a) {
 
     if ($(this).data('id') == 1) {
         $(this).data('id', 2);
-
         $(".box-1 img").attr('src', data_img[0][0].img);
         $(".box-2 img").attr('src', data_img[0][1].img);
         $(".box-3 img").attr('src', data_img[0][2].img);
@@ -93,6 +92,24 @@ $(".click-more-img").click(function (a) {
         $(".box-4 img").attr('src', data_img[1][3].img);
     }
 });
+
+function getDataNews(){
+    return [
+        {
+            title:'123',
+            content:'alkdf',
+            link:'/index.html',
+            img:'https://i-vnexpress.vnecdn.net/2019/03/12/nghi-pham-1984-1552407954.jpg'
+        },
+        {
+            title:'123',
+            content:'alkdf',
+            link:'/index.html',
+            img:'https://i-vnexpress.vnecdn.net/2019/03/12/map-edit-7763-1552378206-8496-1552407954.jpg'
+        }
+
+    ]
+}
 function checkscroll(){
     if ($(window).scrollTop() > 70) {
         $('.menu').addClass('fixed');
@@ -102,8 +119,32 @@ function checkscroll(){
         $('.menu').addClass('hidden');
     }
 }
+
+function setNews(){
+    let data = getDataNews();
+    let k = $(".content-news article").first();
+    k.hide();
+    //k.remove();
+    $.each(data,function(key,v){
+        m = k.clone();
+        m.find('img').attr('src',v.img);
+        m.find('a').attr('href',v.link);
+        m.find('a').text(v.title);
+        m.find('.content-blog').text(v.content);
+        m.attr('data-id',key+1);
+        if(key != 0){
+            m.hide();
+        }else{
+            m.show();
+        }
+        $(".content-news").append(m);
+
+    });
+
+}
 $(document).ready(function(){
     checkscroll();
+    setNews();
 })
 $(window).bind('scroll', function () {
     checkscroll();
